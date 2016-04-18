921426c0af14fe8e49b998aefed3657ec701149e
Deepan Sekar <deepan.sek@gmail.com>
2016-04-17 22:44:27 -0400

    updated

--- final_deepan.sql
+++ final_deepan.sql
@@ -191,3 +191,55 @@ from
         WHERE PID IN (select PID from PLAYERUSERTEAM PL where PL.name = 'Team 2' and PL.user_email='test@test.com')))));
 
 -----------------------------------------------------------------------
+
+/*Player Impact Analyser modified for player value analyser*/
+
+select pid, (bat_play / bat_tot) as bat_stat, (field_play / field_tot) as field_stat, (pitch_play / pitch_tot) as pitch_stat
+from
+  (select avg(stat / tot) as bat_tot
+  from
+    (select (sum(singles) + (2 * sum(doubles)) + (3 * sum(triples)) + (4 * sum(rbi)) + (2 * sum(stolen_bases)) + (0.5 * sum(hits)) - (3 * sum(strikeouts)) + (4 * sum(home_runs))) as stat
+    from BATYEAR A),
+    (select sum(games) as tot
+    from statyear C)),
+  (select avg(stat / tot) as field_tot
+  from
+    (select (sum(putouts) + (1.5 * sum(assists)) - (2 * sum(errors)) + (3 * sum (double_plays))) as stat
+    from FIELDYEAR A),
+    (select sum(games) as tot
+    from statyear C)),
+  (select avg(stat / tot) as pitch_tot
+  from
+    (select (sum(outs) + (5 * sum(shutouts)) - (2 * sum(homeruns)) - sum(walks) + sum(strikeouts)) as stat
+    from PITCHYEAR A),
+    (select sum(games) as tot
+    from statyear C)),
+  ((select pid, avg(stat / tot) as bat_play
+  from
+    (select pid, (sum(singles) + (2 * sum(doubles)) + (3 * sum(triples)) + (4 * sum(rbi)) + (2 * sum(stolen_bases)) + (0.5 * sum(hits)) - (3 * sum(strikeouts)) + (4 * sum(home_runs))) as stat
+    from BATYEAR
+    group by PID) natural join
+    (select pid, sum(games) as tot
+    from statyear
+    group by PID)
+  group by pid) natural join
+  (select pid, avg(stat / tot) as field_play
+  from
+    (select pid, (sum(putouts) + (1.5 * sum(assists)) - (2 * sum(errors)) + (3 * sum (double_plays))) as stat
+    from FIELDYEAR
+    group by PID) natural join
+    (select pid, sum(games) as tot
+    from statyear
+    group by PID)
+  group by pid) natural join
+  (select pid, avg(stat / tot) as pitch_play
+  from
+    (select pid, (sum(outs) + (5 * sum(shutouts)) - (2 * sum(homeruns)) - sum(walks) + sum(strikeouts)) as stat
+    from PITCHYEAR
+    group by PID) natural join
+    (select pid, sum(games) as tot
+    from statyear
+    group by PID)
+  group by pid));
+  
+-------------------------------------------------------------
